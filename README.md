2a2a94eac13110e434592ecbcec50e3d213a6bff
Aman Rawat <ar.rawataman@gmail.com>
2023-12-31 22:16:24 +0530

    created variables.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javascript
+
+This repo contains my javascript code which I have been learning. Its a documented version of my journey.
---
+++ variables.js
@@ -0,0 +1,9 @@
+let var1 = "This is sample string"
+let var2 = 252
+let var3 = false
+
+let datamessage;
+datamessage = 1
+
+console.log(`value of var1 is '${var1}', var2 is ${var2}`);
+console.log(`${datamessage}`);
